16c81e56bd0086d9ad29c156d5f65eacb0ad9350
pkonon <konon.pavel@gmail.com>
2018-08-22 18:34:56 +0300

    - init

--- load-deps.sh
+++ load-deps.sh
@@ -9,3 +9,7 @@ python setup.py build_ext --inplace
 # Download the Pascal VOC dataset:
 curl -O https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
 tar xf VOCtest_06-Nov-2007.tar
+
+mkdir bin
+curl -O https://pjreddie.com/media/files/yolov2.weights
+mv yolov2.weights bin
