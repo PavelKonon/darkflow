8121ed90abc6587e563e825341d62cb976e700e8
pkonon <konon.pavel@gmail.com>
2018-08-22 19:13:59 +0300

    - init

--- demo-camera.sh
+++ demo-camera.sh
@@ -3,7 +3,7 @@
 #./flow --model cfg/yolo-car.cfg --load bin/yolo.weights --demo camera
 
 
-./flow --model cfg/yolov2-car.cfg --load bin/yolov2.weights --train --annotation VOCdevkit/VOC2007/FilteredAnnotations --dataset VOCdevkit/VOC2007/JPEGImages
+./flow --model cfg/yolov2-car.cfg --load bin/yolov2.weights --train --annotation VOCdevkit/VOC2007/FilteredAnnotations --dataset VOCdevkit/VOC2007/JPEGImages --gpu 1
 
 #./flow --model cfg/yolo-car.cfg --load -1 --demo webcam
 
